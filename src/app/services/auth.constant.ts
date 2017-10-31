e1c31fecb9fd3ba2c4b18589f6ed79fdb2a79928
Santiago Gonzalez <hernangt12re3@gmail.com>
2017-10-31 12:49:58 -0500

    Fix #2 to successfully authenticate to the spring server

--- src/app/services/auth.constant.ts
+++ src/app/services/auth.constant.ts
@@ -1,3 +1,3 @@
 export const TOKEN_AUTH_USERNAME = 'testjwtclientid';
-export const TOKEN_AUTH_PASSWORD = 'MaYzkSjmkzPC57L';
+export const TOKEN_AUTH_PASSWORD = 'XY7kmzoNzl100';
 export const TOKEN_NAME = 'access_token';
